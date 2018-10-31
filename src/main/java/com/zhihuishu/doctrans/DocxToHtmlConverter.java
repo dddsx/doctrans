5d7a0f3d98b3e0eb838000333a50fa3f8a671054
lvxiangjun <lvxiangjun@able-elec.com>
2018-10-31 16:42:41 +0800

    优化文件流处理

--- src/main/java/com/zhihuishu/doctrans/DocxToHtmlConverter.java
+++ src/main/java/com/zhihuishu/doctrans/DocxToHtmlConverter.java
@@ -14,14 +14,9 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFPictureData;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class DocxToHtmlConverter {
 
@@ -29,30 +24,18 @@ public class DocxToHtmlConverter {
 
     private static final String IMAGE_PATH = "/data/image/word/media/";
 
-    private static final String DOMNLOAD_PATH = "/data/download/";
-
     /**
-     * 将docx文件识别为html，两个参数需有其一不为null
-     * @param inputStream
+     * 将docx文件转换为带图文的html
+     * @param inputStream docx文件输入流
      * @return html字符串
      */
     public static String docx2html(InputStream inputStream) {
         String htmlResult = "";
-        File docx;
-        try {
-            docx = new File(DOMNLOAD_PATH, UUID.randomUUID().toString() + Constant.EXT_DOCX);
-            FileUtils.copyInputStreamToFile(inputStream, docx);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "";
-        }
-
-        String docxName = docx.getName();
-        String htmlName = HTML_PATH + FilenameUtils.getBaseName(docxName) + Constant.EXT_HTML;
-        File htmlFile = new File(htmlName);
-
-        try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(FileUtils.openOutputStream(htmlFile), "utf-8")){
-            XWPFDocument document = new XWPFDocument(new FileInputStream(docx));
+        File htmlOutputFile = new File(HTML_PATH + UUID.randomUUID().toString() + Constant.EXT_HTML);
+        try (InputStream docxInputStream = inputStream;
+             Writer writer = new OutputStreamWriter(FileUtils.openOutputStream(htmlOutputFile), StandardCharsets.UTF_8))
+        {
+            XWPFDocument document = new XWPFDocument(docxInputStream);
             List<XWPFParagraph> paragraphList = document.getParagraphs();
             Map<String, Shape> shapeMap = new HashMap<>();
             for (XWPFParagraph paragraph : paragraphList) {
@@ -68,54 +51,47 @@ public class DocxToHtmlConverter {
                 }
             }
 
-            // 文档图片内容
+            List<File> imgFileList = new ArrayList<>();
             List<XWPFPictureData> pictures = document.getAllPictures();
             for (XWPFPictureData picture : pictures) {
                 try {
-                    File imgFile = new File(IMAGE_PATH + picture.getFileName());
-                    FileUtils.writeByteArrayToFile(imgFile, picture.getData());
+                    File img = new File(IMAGE_PATH + picture.getFileName());
+                    imgFileList.add(img);
+                    FileUtils.writeByteArrayToFile(img, picture.getData());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
+
             XHTMLOptions options = XHTMLOptions.create();
             options.setImageManager(new ImageManager(new File(IMAGE_PATH), ""));
             options.setFragment(true);
             options.setIgnoreStylesIfUnused(true);
             XHTMLConverter xhtmlConverter = (XHTMLConverter) XHTMLConverter.getInstance();
-            xhtmlConverter.convert(document, outputStreamWriter, options);
+            xhtmlConverter.convert(document, writer, options);
+            htmlResult = FileUtils.readFileToString(htmlOutputFile, "UTF-8");
+            htmlOutputFile.delete();
 
-            htmlResult = FileUtils.readFileToString(htmlFile, "UTF-8");
-
-            File imgDir = new File(IMAGE_PATH);
-            File[] imgs = imgDir.listFiles();
-            for (File imgFile : imgs) {
-                if (!imgFile.isDirectory()) {
-                    String imgName = imgFile.getName();
-                    if(FilenameUtils.isExtension(imgName, Constant.FORMAT_WMF)){
-                        File svgFile = new File(IMAGE_PATH, FilenameUtils.getBaseName(imgName) + Constant.EXT_SVG);
-                        File pngFile = new File(IMAGE_PATH, FilenameUtils.getBaseName(imgName) + Constant.EXT_PNG);
-                        ImgConverter.convertWmf2Svg(imgFile, svgFile);
-                        ImgConverter.convertSvg2Png(svgFile, pngFile);
-                        String imgOssUrl = MyFileUtil.uploadFileToOSS(pngFile);
-
-                        svgFile.delete();
-                        pngFile.delete();
-
-                        Shape shape = shapeMap.get(imgName);
-                        String imgRefPlaceholder = XWPFUtils.createRefPlaceholder(shape.getRef());
-                        htmlResult = htmlResult.replace(imgRefPlaceholder, createImgTag(imgOssUrl, shape.getStyle()));
-                    } else {
-                        String imgOssUrl = MyFileUtil.uploadFileToOSS(imgFile);
-                        String imgHtmlUrl = File.separator + imgFile.getName();
-                        htmlResult = htmlResult.replace(imgHtmlUrl, imgOssUrl);
-                    }
-                    imgFile.delete();
+            for (File imgFile : imgFileList) {
+                String imgName = imgFile.getName();
+                if(FilenameUtils.isExtension(imgName, Constant.FORMAT_WMF)){
+                    File svgFile = new File(IMAGE_PATH, FilenameUtils.getBaseName(imgName) + Constant.EXT_SVG);
+                    File pngFile = new File(IMAGE_PATH, FilenameUtils.getBaseName(imgName) + Constant.EXT_PNG);
+                    ImgConverter.convertWmf2Svg(imgFile, svgFile);
+                    ImgConverter.convertSvg2Png(svgFile, pngFile);
+                    String imgOssUrl = MyFileUtil.uploadFileToOSS(pngFile);
+                    svgFile.delete();
+                    pngFile.delete();
+                    Shape shape = shapeMap.get(imgName);
+                    String imgRefPlaceholder = XWPFUtils.createRefPlaceholder(shape.getRef());
+                    htmlResult = htmlResult.replace(imgRefPlaceholder, createImgTag(imgOssUrl, shape.getStyle()));
+                } else {
+                    String imgOssUrl = MyFileUtil.uploadFileToOSS(imgFile);
+                    String imgHtmlUrl = File.separator + imgFile.getName();
+                    htmlResult = htmlResult.replace(imgHtmlUrl, imgOssUrl);
                 }
+                imgFile.delete();
             }
-            //删除本地文件
-            htmlFile.delete();
-            docx.delete();
         } catch (Exception e) {
             e.printStackTrace();
         }
